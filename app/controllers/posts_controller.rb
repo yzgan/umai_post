a3be2e4e3d3ac7a69ca35bb09e2881fe9f998a84
Gan Yi Zhong <ganyz@bookdoc.com>
2021-06-19 18:31:24 +0800

    create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     render json: @user
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_ip = request.remote_ip
+    @post.user = @user
+
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   validates :username, :password, presence: true
 
   def self.find_from_jwt(token)
